00ac921a684334a05fa970ad461cc1831d64fed7
miad <adrian.michel@stud.abbts.ch>
2021-04-23 09:02:24 +0200

    den Spring Starter ins Repo eingefügt

---
+++ starter#1/starter-1/src/main/java/ch/abbts/nds/swe/swdt/starter1/Application.java
@@ -0,0 +1,13 @@
+package ch.abbts.nds.swe.swdt.starter1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
