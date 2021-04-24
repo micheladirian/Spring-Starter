569e0379a1eb0b22566ce9002ada4a0b6bc49067
miad <adrian.michel@stud.abbts.ch>
2021-04-24 16:00:10 +0200

    Leer (Start)

--- starter_1/src/main/java/ch/abbts/nds/swe/swdt/starter1/Application.java
+++ starter_1/src/main/java/ch/abbts/nds/swe/swdt/starter1/Application.java
@@ -1,13 +1,20 @@
+// Packet name
 package ch.abbts.nds.swe.swdt.starter1;
 
-		import java.util.Arrays;
+// Import von bereits existierenden Klassen
+import java.util.Arrays;
 
-		import org.springframework.boot.CommandLineRunner;
-		import org.springframework.boot.SpringApplication;
-		import org.springframework.boot.autoconfigure.SpringBootApplication;
-		import org.springframework.context.ApplicationContext;
-		import org.springframework.context.annotation.Bean;
+// Import von bereits existierenden spring Klassen
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
+//Die Spring Boot-Annotation wird verwendet, um eine Konfigurationsklasse zu markieren,
+// die eine oder mehrere @Bean-Methoden deklariert und außerdem die Autokonfiguration und das Scannen von Komponenten auslöst.
+// Es ist dasselbe wie das Deklarieren einer Klasse mit den Annotationen
+// @Configuration, @EnableAutoConfiguration und @ComponentScan.
 @SpringBootApplication
 public class Application {
 
@@ -15,6 +22,8 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+
+	// Wird nicht verwendet.....!!!
 	@Bean
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return args -> {
--- starter_1/src/main/java/ch/abbts/nds/swe/swdt/starter1/HelloController.java
+++ starter_1/src/main/java/ch/abbts/nds/swe/swdt/starter1/HelloController.java
@@ -1,13 +1,26 @@
+// Packet name
 package ch.abbts.nds.swe.swdt.starter1;
 
+// Import von bereits existierenden Klassen
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+//In der @RestController Annotation werden die zu verwendung stehenden WEB-Abfragen Hinterlegt
 @RestController
 public class HelloController {
 
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot \n \t Hallo Adi, das ist von @RequestMapping ()";
+    }
+
+    @RequestMapping("/xx")
+    public String Index_1() {
+        return "Das ist ein anderer Request";
+    }
+
+    @RequestMapping("/xx/xx")
+    public String Index_2() {
+        return "Test: Greeadsssss jajajajajajajaja estMapping ()";
     }
 }
